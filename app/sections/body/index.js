555e394f1754ec63dfe169e4887f4f60e20ff847
Time_Terminal <vishesh.thanki@gmail.com>
2019-04-13 02:24:52 -0400

    Add users dropdown which shows fetched users

--- app/sections/body/index.js
+++ app/sections/body/index.js
@@ -7,6 +7,7 @@ class Body extends React.Component {
   constructor() {
     super();
     this.state = {
+      users: [],
       selectedUser: null
     };
   }
@@ -19,11 +20,36 @@ class Body extends React.Component {
     });
   }
 
+  displayUsers() {
+    const { users } = this.state;
+    if (users.length === 0) {
+      return <option disabled>Loading Users...</option>;
+    } else {
+      return users.map(user => {
+        return (
+          <option key={user.id} value={user.id}>
+            {user.name}
+          </option>
+        );
+      });
+    }
+  }
+
+  fetchAlbums(userId) {
+  }
+
   render() {
     console.log(this.state, "state");
     return (
       <React.Fragment>
         <h1 className={css.title}>Body</h1>
+        <div className={"css.users-dropdown"}>
+          <label>Users:</label>
+          <select onChange={e => this.fetchAlbums(e.target.value)}>
+            <option>Select User</option>
+            {this.displayUsers()}
+          </select>
+        </div>
       </React.Fragment>
     );
   }
