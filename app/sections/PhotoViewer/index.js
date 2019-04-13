c62993b38a44ae6d1b38e9d4b8492c83c870d75c
Time_Terminal <vishesh.thanki@gmail.com>
2019-04-13 17:57:59 -0400

    Make carousel switch between photos

--- app/sections/PhotoViewer/index.js
+++ app/sections/PhotoViewer/index.js
@@ -6,16 +6,79 @@ import Title from "../../sharedComponents/title";
 import Card from "./Card";
 
 class PhotoViewer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentPhotoNumber: 1
+    };
+
+    this.shiftPhotos = this.shiftPhotos.bind(this);
+  }
+
+  shiftPhotos(direction) {
+    const { currentPhotoNumber } = this.state;
+    let newIndex = currentPhotoNumber;
+
+    if (direction === "forward") {
+      newIndex = currentPhotoNumber + 1;
+    } else {
+      newIndex = currentPhotoNumber - 1;
+    }
+
+    this.setState({
+      currentPhotoNumber: newIndex
+    });
+  }
+
+  renderButtons(albumPhotos, currentPhotoNumber) {
+    return (
+      <div className={css.buttons}>
+        <button
+          className={css.buttons__previous}
+          onClick={() => this.shiftPhotos("backward")}
+          disabled={albumPhotos.indexOf(currentPhotoNumber) === 0}
+        >
+          Previous
+        </button>
+        <button
+          className={css.buttons__next}
+          onClick={() => this.shiftPhotos("forward")}
+          disabled={
+            albumPhotos.indexOf(currentPhotoNumber) === albumPhotos.length - 1
+          }
+        >
+          Next
+        </button>
+      </div>
+    );
+  }
+
   render() {
     const { title, albumPhotos } = this.props;
+    const { currentPhotoNumber } = this.state;
+    const size = 600;
 
     return (
       <div className={css.photos_container}>
         {albumPhotos.length !== 0 && (
           <React.Fragment>
             <Title title={title} />
-            <div className={css.cards_slider}>
-              <div className={css.cards_slider_wrapper}>
+            {/* Buttons */}
+            {this.renderButtons(albumPhotos, currentPhotoNumber)}
+            {/* Carousel */}
+            <div className={css.carousel}>
+              <div
+                className={css.carousel__slider}
+                style={{
+                  transform: `translateX(${-size * currentPhotoNumber}px)`
+                }}
+              >
+                <Card
+                  id="carousel__lastClone"
+                  imageSource={albumPhotos[albumPhotos.length - 1].url}
+                  imageTitle={albumPhotos[albumPhotos.length - 1].title}
+                />
                 {albumPhotos.map(photo => {
                   return (
                     <Card
@@ -25,6 +88,11 @@ class PhotoViewer extends React.Component {
                     />
                   );
                 })}
+                <Card
+                  id="carousel__firstClone"
+                  imageSource={albumPhotos[0].url}
+                  imageTitle={albumPhotos[0].title}
+                />
               </div>
             </div>
           </React.Fragment>
