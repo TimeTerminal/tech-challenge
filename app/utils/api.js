0f66d9106931f8e7351a1423b839ce64415ac4db
Time_Terminal <vishesh.thanki@gmail.com>
2019-04-13 04:42:02 -0400

    Fetch full Album data with photos

--- app/utils/api.js
+++ app/utils/api.js
@@ -7,5 +7,38 @@ module.exports = {
     return axios.get(`${pathRoot}/users`).then(response => {
       return response.data;
     });
+  },
+  fetchAlbumsWithArt: async function(userId) {
+    try {
+      const albums = await axios
+        .get(`${pathRoot}/albums`, {
+          params: {
+            userId
+          }
+        })
+        .catch(e => {
+          console.log(error);
+        });
+
+      const fullAlbumData = Promise.all(
+        albums.data.map(albumsInfo => {
+          return axios
+            .get(`${pathRoot}/photos`, {
+              params: {
+                albumId: albumsInfo.id
+              }
+            })
+            .catch(e => {
+              console.log(error);
+            });
+        })
+      ).then(albumImages => {
+        return albumImages.map(albumImage => albumImage.data);
+      });
+
+      return fullAlbumData;
+    } catch (error) {
+      console.error(error);
+    }
   }
 };
